8204900d384ef9db4b2e93fb3563adc8a866109d
MarQ <marvinpescos@gmail.com>
2026-01-27 12:15:12 +0800

    feat: added slugging

--- backend/app/infrastructure/database/user_repository.py
+++ backend/app/infrastructure/database/user_repository.py
@@ -1,6 +1,8 @@
+from slugify import slugify
 from typing import List
 from sqlalchemy import select, and_
 import uuid
+
 
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -59,8 +61,44 @@ class UserScopeRepository(BaseRepository[ModelType]):
                 f"Failed to get all by user id {self.model_name}",
                 original_error=e
             )
+    
+    # TODO pagination
+    
+    async def get_by_slug(self, slug: str, user_id: uuid.UUID | None = None) -> ModelType | None:
+        """Get record bu slug"""
+        try:
+            query = select(self.model).where(self.model.slug == slug)
 
+            if user_id:
+                query = query.where(self.model.user_id == user_id)
+                
+            result = await self.session.execute(query)
+            return result.scalar_one_or_none()
 
+        except SQLAlchemyError as e:
+            log.error(
+                "database.error",
+                model=self.model_name,
+                operation="get_by_slug",
+                error=str(e)
+            )
+            raise DatabaseError(
+                f"Failed to get by slug {self.model_name}",
+                original_error=e
+            )
 
+    async def generate_unique_slug(self, base_text: str, user_id: uuid.UUID | None = None) -> str:
+        base_slug = slugify(base_text)
+        slug = base_slug
+        counter = 1
 
+        while True:
+            existing = await self.get_by_slug(slug, user_id)
 
+            if not existing: #if it not exist matik break then return ang slug
+                break
+            
+            slug = f"{base_slug}-{counter}"
+            counter +=1
+        
+        return slug
