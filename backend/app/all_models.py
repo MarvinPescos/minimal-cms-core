f7444c92c162693fa802224fd0cc4867f0c29f21
MarQ <marvinpescos@gmail.com>
2026-02-16 22:12:15 +0800

    feat: add tenant and tenant routes

--- backend/app/all_models.py
+++ backend/app/all_models.py
@@ -3,6 +3,6 @@ from app.infrastructure.database import Base
 from app.features.users.models import User
 from app.features.events.models import Event
 from app.features.gallery.models import Album, Image
-
+from app.features.tenants.models import Tenant, TenantMembers
 
 __all__ = ["Base"]
